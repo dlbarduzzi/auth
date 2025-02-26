34270fd1d28330adcfdb67587e482e34602e7457
Dylan Barduzzi <dlbarduzzi@gmail.com>
2025-02-26 11:31:16 -0600

    Setup eslint and prettier

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,22 @@
+export default function Page() {
+  return (
+    <div>
+      <section aria-labelledby="homepage-header">
+        <h1 id="homepage-header" className="sr-only">
+          Auth Homepage.
+        </h1>
+      </section>
+      <div className="max-w-2xl space-y-4 p-4">
+        <div className="border border-gray-200 px-5 py-4">
+          <h1 className="font-bold">Fun Fact</h1>
+          <p className="pt-2">
+            In the United States, a person must be at least 35 to be President or Vice
+            President, 30 to be a Senator, or 25 to be a Representative, as specified in
+            the U.S. Constitution. Most states in the U.S. also have age requirements
+            for the offices of Governor, State Senator, and State Representative.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
