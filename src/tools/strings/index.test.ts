23fa45d463ef5b1825a5355ff0d722ef9b339e5e
Dylan Barduzzi <dlbarduzz@gmail.com>
2025-07-21 09:56:58 -0500

    Add cookie serialization

--- src/tools/strings/index.test.ts
+++ src/tools/strings/index.test.ts
@@ -1,11 +1,18 @@
-import { lowercase } from "./index"
 import { describe, expect, it } from "vitest"
+import { lowercase, capitalize } from "./index"
 
 describe("lowercase string", () => {
   it("should lowercase string", () => {
-    expect(lowercase("Hello")).toBe("hello")
-    expect(lowercase("HELLO")).toBe("hello")
-    expect(lowercase("Hello, World!")).toBe("hello, world!")
+    expect(lowercase("Hello, world!")).toBe("hello, world!")
     expect(lowercase("HELLO, WORLD!")).toBe("hello, world!")
+    expect(lowercase("Hello, World!")).toBe("hello, world!")
+  })
+})
+
+describe("capitalize string", () => {
+  it("should capitalize string", () => {
+    expect(capitalize("hello, world!")).toBe("Hello, world!")
+    expect(capitalize("HELLO, WORLD!")).toBe("HELLO, WORLD!")
+    expect(capitalize("hello, World!")).toBe("Hello, World!")
   })
 })
