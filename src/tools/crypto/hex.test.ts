3758739b638b1e8c060c0e2dc5574ef6438b4b66
Dylan Barduzzi <dlbarduzz@gmail.com>
2025-07-22 09:45:10 -0500

    Add hex decode

--- src/tools/crypto/hex.test.ts
+++ src/tools/crypto/hex.test.ts
@@ -3,8 +3,62 @@ import { Buffer } from "node:buffer"
 import { describe, expect, it } from "vitest"
 
 describe("hex encode", () => {
-  it("should encode an ArrayBuffer to hexadecimal", () => {
-    const input = new Uint8Array([72, 101, 108, 108, 111]).buffer
+  it("should encode a string to hexadecimal", () => {
+    const input = "Hello, World!"
     expect(hex.encode(input)).toBe(Buffer.from(input).toString("hex"))
+  })
+
+  it("should encode an ArrayBuffer to hexadecimal", () => {
+    const input = new TextEncoder().encode("Hello")
+    expect(hex.encode(input)).toBe(Buffer.from(input).toString("hex"))
+  })
+
+  it("should encode a TypedArray to hexadecimal", () => {
+    const input = new Uint8Array([72, 101, 108, 108, 111])
+    expect(hex.encode(input)).toBe(Buffer.from(input).toString("hex"))
+  })
+})
+
+describe("hex decode", () => {
+  it("should decode a hexadecimal string to its original value", () => {
+    const input = "Hello, World!"
+    expect(hex.decode(Buffer.from(input).toString("hex"))).toBe(input)
+  })
+
+  it("should decode a hexadecimal string to binary data", () => {
+    const input = "Hello"
+    expect(hex.decode(Buffer.from(input).toString("hex"))).toBe(input)
+  })
+
+  it("should decode a hexadecimal string to bytes data", () => {
+    const input = "Hello"
+    const encoded = hex.decode(Buffer.from(input).toString("hex"), "bytes")
+    expect(encoded.toString()).toBe("72,101,108,108,111")
+  })
+
+  it("should throw an error for an invalid length string", () => {
+    const input = "123"
+    expect(() => hex.decode(input)).toThrowError("Invalid hexadecimal string")
+  })
+
+  it("should throw an error for a non-hexadecimal string", () => {
+    const input = "xxxx"
+    expect(() => hex.decode(input)).toThrowError("Invalid hexadecimal string")
+  })
+})
+
+describe("hex encode and decode", () => {
+  it("should decode original string", () => {
+    const input = "Hello, World!"
+    const encoded = hex.encode(input)
+    const decoded = hex.decode(encoded)
+    expect(decoded).toBe(input)
+  })
+
+  it("should handle empty strings", () => {
+    const input = ""
+    const encoded = hex.encode(input)
+    const decoded = hex.decode(encoded)
+    expect(decoded).toBe(input)
   })
 })
--- src/tools/crypto/hex.ts
+++ src/tools/crypto/hex.ts
@@ -1,9 +1,36 @@
+import type { TypedArray } from "./types"
+
 export const hex = {
-  encode: (data: ArrayBuffer) => {
-    const buffer = new Uint8Array(data)
-    const result = Array.from(buffer)
+  encode: (data: string | ArrayBuffer | TypedArray) => {
+    const value = typeof data === "string"
+      ? new TextEncoder().encode(data)
+      : data
+    if (value.byteLength === 0) {
+      return ""
+    }
+    const bytes = new Uint8Array(value)
+    const result = Array.from(bytes)
       .map(byte => byte.toString(16).padStart(2, "0"))
       .join("")
     return result
+  },
+  decode: (data: string, format: "bytes" | "string" = "string") => {
+    if (!data) {
+      return ""
+    }
+    if (data.length % 2 !== 0) {
+      throw new Error("Invalid hexadecimal string")
+    }
+    if (!/^[0-9a-f]+$/.test(data)) {
+      throw new Error("Invalid hexadecimal string")
+    }
+    const result = new Uint8Array(data.length / 2)
+    for (let i = 0; i < data.length; i += 2) {
+      result[i / 2] = Number.parseInt(data.slice(i, i + 2), 16)
+    }
+    if (format === "bytes") {
+      return result
+    }
+    return new TextDecoder().decode(result)
   },
 }
