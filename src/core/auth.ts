8e50619da980927ac5975d5b68c5a25e23d4f152
Dylan Barduzzi <dlbarduzz@gmail.com>
2025-07-25 10:03:43 -0500

    Parse json input

--- src/core/auth.ts
+++ src/core/auth.ts
@@ -1,10 +1,14 @@
 import type { UserSchema, SessionSchema } from "@/db/schemas"
 
+import z from "zod"
+
+import { status } from "@/tools/http/status"
+
 import { env } from "./env"
 import { newApp } from "./app"
-
-import { createResponse } from "./response"
+import { getJsonInput } from "./request"
 import { getCachedCookie, setSessionCookie } from "./cookie"
+import { sendBadRequest, sendInternalServerError } from "./response"
 
 const app = newApp()
 
@@ -28,7 +32,36 @@ const session: SessionSchema = {
   updatedAt: new Date(Date.UTC(2025, 6, 20, 12, 10, 15, 900)),
 }
 
+const INVALID_JSON_REQUEST = "Invalid JSON request."
+const INVALID_JSON_PAYLOAD = "Invalid JSON payload."
+
+const loginSchema = z.strictObject({
+  email: z
+    .email("Not a valid email")
+    .trim()
+    .min(1, "Email is required"),
+  password: z
+    .string({ error: "Password is required" })
+    .trim()
+    .min(1, "Password is required"),
+  rememberMe: z.boolean().optional().default(undefined),
+})
+
 app.post("/login", async ctx => {
+  const input = await getJsonInput(ctx.req)
+
+  switch (input.code) {
+    case status.code.badRequest:
+      return sendBadRequest(INVALID_JSON_REQUEST)
+    case status.code.internalServerError:
+      return sendInternalServerError(ctx, input.error)
+  }
+
+  const parsed = loginSchema.safeParse(input.data)
+  if (!parsed.success) {
+    return sendBadRequest(INVALID_JSON_PAYLOAD, z.treeifyError(parsed.error).properties)
+  }
+
   await setSessionCookie({
     data: { user, session },
     headers: { set: ctx.header, get: ctx.req.raw.headers },
@@ -40,10 +73,6 @@ app.post("/login", async ctx => {
 app.get("/session", async ctx => {
   const data = await getCachedCookie(env.AUTH_SECRET, ctx.req.raw.headers)
   return ctx.json({ ok: "Cool session", data })
-})
-
-app.get("/test", async () => {
-  return createResponse({ ok: true }, 200)
 })
 
 export const authRoutes = app
--- src/core/response.ts
+++ src/core/response.ts
@@ -1,18 +1,73 @@
-import type { StatusCode } from "@/tools/http/status"
+import type { AppContext } from "./types"
 
-import { toStatusText } from "@/tools/http/status"
+import { env } from "./env"
+import { status, toStatusText } from "@/tools/http/status"
 
-export function createResponse(
-  obj: object,
-  status: StatusCode,
-  headers?: ResponseInit["headers"],
-): Response {
-  return new Response(JSON.stringify(obj), {
-    status,
-    statusText: toStatusText(status),
-    headers: {
-      "Content-Type": "application/json",
-      ...headers,
+const JSON_HEADER = { "Content-Type": "application/json" }
+
+function safeJsonString(obj: object) {
+  try {
+    return JSON.stringify(obj)
+  }
+  catch (error) {
+    if (env.NODE_ENV === "development") {
+      console.error(error)
+    }
+    else {
+      // This should never happen
+      return "ERROR: safeJsonString failed"
+    }
+  }
+}
+
+export function sendBadRequest(message: string, errors?: unknown): Response {
+  const statusCode = status.code.badRequest
+  const statusText = toStatusText(statusCode)
+
+  return new Response(safeJsonString({
+    ok: false,
+    status: {
+      code: statusCode,
+      text: statusText,
     },
+    details: {
+      errors,
+      message,
+    },
+  }), {
+    status: statusCode,
+    statusText,
+    headers: { ...JSON_HEADER },
+  })
+}
+
+export function sendInternalServerError(ctx: AppContext, error: unknown): Response {
+  const statusCode = status.code.internalServerError
+  const statusText = toStatusText(statusCode)
+
+  ctx.var.logger.error(error instanceof Error
+    ? error.message
+    : "uncaught error", {
+    path: ctx.req.path,
+    method: ctx.req.method,
+  })
+
+  if (env.NODE_ENV === "development") {
+    console.error(error)
+  }
+
+  return new Response(safeJsonString({
+    ok: false,
+    status: {
+      code: statusCode,
+      text: statusText,
+    },
+    details: {
+      message: "Something went wrong while processing this request.",
+    },
+  }), {
+    status: statusCode,
+    statusText,
+    headers: { ...JSON_HEADER },
   })
 }
